66640fec7524fad767274d12712d67222c151f5e
Daniël <15849319+abcdan@users.noreply.github.com>
2024-06-09 15:53:10 +0200

    add version config

---
+++ config/version.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  /**
+   * DO NOT TOUCH. This is the version of the application.
+   * It'll be updated automatically by the owner of the repository.
+   *
+   * 0.0.0X - Small changes
+   * 0.0.X - Bug fixes
+   * 0.X.0 - New features
+   * X.0.0 - Major changes
+   *
+   * We use SemVer for versioning.
+   */
+  version: "0.0.1A",
+};
