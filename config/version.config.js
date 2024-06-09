d1976012481b2653438658cc5ae7230ea190dc41
Daniël <15849319+abcdan@users.noreply.github.com>
2024-06-09 15:53:18 +0200

    add version config

--- config/version.config.js
+++ config/version.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   /**
    * DO NOT TOUCH. This is the version of the application.
-   * It'll be updated automatically by the owner of the repository.
+   * It'll be updated by the owner of the repository.
    *
    * 0.0.0X - Small changes
    * 0.0.X - Bug fixes
